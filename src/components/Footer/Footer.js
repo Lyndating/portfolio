7eec4c936bcccd7f4ac58dccdd9c5e2792b25119
Ting Luo <shirleyayu@hotmail.com>
2022-06-03 12:47:45 +1000

    setup Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
