6a4f6eceed1f07abef2227b71f78d1ff327f6003
Ting Luo <shirleyayu@hotmail.com>
2022-06-04 13:43:43 +1000

    set up tech component

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Section, SectionTitle, SectionDivider, SectionText } from "../../styles/GlobalComponents/Section";
+import { List, ListContainer, ListTitle } from "./TechnologiesStyles";
+
 const Technologies = () =>  (
   <div>
     Technologies
