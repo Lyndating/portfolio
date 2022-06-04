3112733b1ce7f7054e7891e02b066b5a79b6ef09
Ting Luo <shirleyayu@hotmail.com>
2022-06-04 13:46:50 +1000

    insert HomeSection in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Layout } from '../layout/Layout';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
+import HomeSection from '../components/Home/HomeSection';
+import { Section } from '../styles/GlobalComponents/Section';
 
-const Home = () => {
+const Home = (props) => {
   return (
     
       <Layout>
+        <Section grid>
+          <HomeSection/>
+        </Section> 
         <Projects/>
         <Technologies/>
       </Layout>
