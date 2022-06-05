cea77bb76b0614a6d9eadba11cd4783934aff010
Ting Luo <shirleyayu@hotmail.com>
2022-06-05 12:51:00 +1000

    setup About component

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents/Section';
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About;
