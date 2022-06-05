67970b6ac42a8fb5f9869f483e3c91bc887d25e0
Ting Luo <shirleyayu@hotmail.com>
2022-06-05 12:52:47 +1000

    setup Tech component, icons and styles

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,11 +1,92 @@
 import React from "react";
 import { Section, SectionTitle, SectionDivider, SectionText } from "../../styles/GlobalComponents/Section";
-import { List, ListContainer, ListTitle } from "./TechnologiesStyles";
+import { List, ListContainer, ListItems, ListItem, ListTitle, ListIcon } from "./TechnologiesStyles";
+import {DiRuby, DiReact, DiRor,DiPython,DiPostgresql, DiNodejsSmall,DiMongodb, DiJavascript, DiJqueryLogo,DiHtml5,DiFirebase,DiCss3,DiBootstrap} from 'react-icons/di';
+import {RiVuejsFill} from 'react-icons/ri';
+import {SiDocker, SiAmazonaws} from 'react-icons/si';
+
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="technology">
+    <SectionDivider/>
+    <br/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web developement world. From back-end to Design and still keep learning new ones.
+    </SectionText>
+      <List>
+        <ListItems>
+          <ListItem>
+            <DiHtml5 size="8rem"/>
+            <ListTitle>HTML</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiCss3 size="8rem"/>
+          <ListTitle>CSS</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiJavascript size="8rem"/>
+          <ListTitle>JavaScript</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiRuby size="8rem"/>
+          <ListTitle>Ruby</ListTitle>
+          </ListItem>
+        </ListItems>
+        <ListItems>
+          <ListItem>
+            <DiRor size="8rem"/>
+          <ListTitle>Rails</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiJqueryLogo size="8rem"/>
+          <ListTitle>JQuery</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiReact size="8rem"/>
+          <ListTitle>React</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiNodejsSmall size="8rem"/>
+          <ListTitle>Node</ListTitle>
+          </ListItem>
+        </ListItems>
+        <ListItems>
+          <ListItem>
+            <RiVuejsFill size="8rem"/>
+            <ListTitle>Vue</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiPostgresql size="8rem"/>
+          <ListTitle>PostgresSQL</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiBootstrap size="8rem"/>
+          <ListTitle>Bootstrap</ListTitle>
+          </ListItem>
+          <ListItem>
+            <DiFirebase size="8rem"/>
+          <ListTitle>Firebase</ListTitle>
+          </ListItem>
+        </ListItems>
+        <ListItems>
+          <ListItem coming>
+            <DiPython size="8rem"/>
+          <ListTitle>Python-Learning</ListTitle>
+          </ListItem>
+          <ListItem coming>
+            <SiDocker size="8rem"/>
+          <ListTitle>Docker-Learning</ListTitle>
+          </ListItem>
+          <ListItem coming>
+            <SiAmazonaws size="8rem"/>
+            <ListTitle>Coming Soon</ListTitle>
+          </ListItem>
+        </ListItems>
+      </List>
+
+  </Section>
 );
 
 export default Technologies;
--- src/components/Technologies/TechnologiesStyles.js
+++ src/components/Technologies/TechnologiesStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const List = styled.ul`
     list-style-type: none;
     display: grid;
-    grid-template-columns: repeat(3,1fr);
+    grid-template-columns: repeat(1,1fr);
     gap: 40px;
     margin: 3rem 0;
     
@@ -35,7 +35,7 @@ export const ListContainer = styled.div`
 `
 
 export const ListTitle = styled.h4`
-    font-size: 30px;
+    font-size: 25px;
     font-weight: 600;
     line-height: 30px;
     color: #fff;
@@ -54,4 +54,53 @@ export const ListTitle = styled.h4`
         letter-spacing: 0.02em;
     }
 
+`
+
+export const ListParagraph = styled.p`
+    font-size: 18px;
+    line-height: 30px;
+    color: rgba(255,255,255, 0.75);
+
+    @media ${props=> props.theme.breakpoints.md} {
+        font-size: 15px;
+        line-height: 22px;
+    }
+
+    @media ${props=> props.theme.breakpoints.sm} {
+        font-size: 14px;
+        line-height: 20px;
+    }
+`
+
+export const ListItems = styled.li`
+    max-width: 90vw;
+    display:flex;
+    flex-direction: row;
+    height: 120px;
+    justify-content: space-evenly;
+`
+
+export const ListItem = styled.div`
+    width: ${props=> props.coming? "260px": "170px"};
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+
+    @media ${props=> props.theme.breakpoints.md} {
+        max-width: 200px;
+    }
+
+    @media ${props => props.theme.breakpoints.sm} {
+        margin-bottom: 14px;
+        max-width: 320px;
+        flex-direction: row;
+    }
+`
+
+export const ListIcon = styled.div`
+    display:flex;
+    width: 100%;
+    height: auto;
+    max-height: 80px;
+    margin-bottom: 10px;
 `
