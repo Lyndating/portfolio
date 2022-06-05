a0aeaa533f27d76c6cae1cf0a8cef0ddda628160
Ting Luo <shirleyayu@hotmail.com>
2022-06-05 12:48:44 +1000

    add background image but not used yet

--- src/styles/global.js
+++ src/styles/global.js
@@ -16,7 +16,8 @@ const GlobalStyle = createGlobalStyle`
     body{
         font-family: ${props=> props.theme.fonts.main};
         font-size: 1.6rem;
-        background: ${props=> props.theme.colors.background1};
+        
+        background: ${props => props.theme.colors.background1};
         color: ${props=> props.theme.colors.primary1};
         cursor: default;
     }
