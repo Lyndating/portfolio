f13b4b9ba3aa4b0847d347e640cf9ba7ad135296
Ting Luo <shirleyayu@hotmail.com>
2022-06-06 21:25:02 +1000

    adjust default style data

--- src/themes/default.js
+++ src/themes/default.js
@@ -6,14 +6,18 @@ export default {
   },
   // Colors for layout
   colors: {
+    // 1 is for dark mode, 2 is for light mode
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
+    primary2: "#0F1624",
+    background1: "linear-gradient(270deg,#c79d13 0%, #d84792 100%)",
+    background2: "linear-gradient(270deg,#945DD6 0%, #13ADC7 100%)",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
-    background2: "hsl(232.7,27.3%,23.7%)",
+    
   },
   // Breakpoints for responsive design
   breakpoints: {
+    xs: 'screen and (max-width: 490px)',
     sm: 'screen and (max-width: 640px)',
     md: 'screen and (max-width: 768px)',
     lg: 'screen and (max-width: 1024px)',
