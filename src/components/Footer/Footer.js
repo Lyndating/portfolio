f05cb2e42fb1a9942ce79b05aac1b2f2256895f3
Ting Luo <shirleyayu@hotmail.com>
2022-06-06 21:21:53 +1000

    add Footer and links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-
-import { SocialIcons } from '../Header/HeaderStyles';
+import { AiFillGithub, AiFillLinkedin,AiOutlineHeart } from 'react-icons/ai';
+import { FooterLi,FooterLinkList,FooterLink, FooterContainer,SocialIconsContainer, Small} from './FooterStyles';
+import {SocialIcons} from '../Header/HeaderStyles'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterContainer>
+        <FooterLinkList>
+    <SocialIconsContainer>
+        <FooterLi>
+            <FooterLink href='https://github.com/Lyndating' target="_blank">GitHub</FooterLink>
+        </FooterLi>
+        <FooterLi>
+            <FooterLink href='https://www.linkedin.com/in/lynn-ting-luo/' >LinkedIn</FooterLink>
+        </FooterLi>
+        <FooterLi>
+            <FooterLink href='mailto:lynn.ting.luo@gmail.com'>Email</FooterLink>
+        </FooterLi>
+        <FooterLi>
+            <FooterLink href='/resume-Ting-Luo.pdf'download>Resume</FooterLink>
+        </FooterLi>
+    </SocialIconsContainer>
+    </FooterLinkList>
+        <Small>
+            &copy; 2022-{new Date().getFullYear()} All Rights Reserved, Made with{' '} 💗 by &lt;Ting&gt;
+        </Small>
+    </FooterContainer>
   );
 };
 
