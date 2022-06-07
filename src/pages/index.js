10e54267f688d7e43f0b891ca19e127ee8998f00
Ting Luo <shirleyayu@hotmail.com>
2022-06-07 10:20:30 +1000

    set up document in page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Technologies from '../components/Technologies/Technologies';
 import HomeSection from '../components/Home/HomeSection';
 import About from '../components/About/About';
 import { Section } from '../styles/GlobalComponents/Section';
-
+import ScrollButton from '../components/ScrollButton/ScrollButton';
 const Home = (props) => {
   return (
     
@@ -13,7 +13,9 @@ const Home = (props) => {
         <Section grid>
           <HomeSection/>
         </Section> 
+        <br/>
         <About/>
+        <br/>
         <Projects/>
         <br/>
         <Technologies/>
