d76db24f8606b8ef0e1fd709a396f8e31ffe9dd3
Ting Luo <shirleyayu@hotmail.com>
2022-06-07 10:19:38 +1000

    add scrollbutton func & style

---
+++ src/components/ScrollButton/ScrollButton.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import {BsArrowUpCircle} from 'react-icons/bs';
+import { TopButton} from './ScrollButtonStyles';
+
+
+const ScrollButton = () => {
+    const [visable, setVisable] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll", ()=>{
+            if (window.scrollY > 200) {
+                setVisable(true);
+            }else {
+                setVisable(false);
+            }
+        })
+    },[]);
+
+    const goToTop = ()=>{
+        window.scrollTo({
+            top:0,
+            behavior:"smooth",
+        });
+    }
+
+  return (
+    <TopButton>
+        {" "}
+        {visable && (
+            <BsArrowUpCircle
+                style={{position:"fixed",bottom:"40px",right:"25px",zIndex:"21", color:"white" , cursor:"pointer",
+                ainimation:"movebtn 3s ease-in-out infinite", transition:"all 0.5s ease-in-out", 
+                }}
+                size="4rem"
+                onClick={goToTop}
+            />
+        )}{" "}
+    </TopButton>
+  )
+}
+
+export default ScrollButton;
