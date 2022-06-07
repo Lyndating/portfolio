2fab2a5394a55a87ee524662971da73c7d644dd6
Ting Luo <shirleyayu@hotmail.com>
2022-06-07 10:19:04 +1000

    add margin in home page

--- src/components/Home/HomeSection.js
+++ src/components/Home/HomeSection.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { Section, SectionTitle, SectionText } from '../../styles/GlobalComponents/Section';
 import Button from '../../styles/GlobalComponents/Button';
 import { MainSection } from './HomeStyles';
+import {AiFillSmile } from 'react-icons/ai';
 
 const HomeSection = (props) => (
     <Section row nopadding marginbottom>
         <MainSection>
+            <br/>
+            <br/>
             <SectionTitle main center>
                 Hello <br/>
-                I am Ting Luo
+                I am Ting Luo 
             </SectionTitle>
             <SectionText>
-                Software Engineer | Love to learn & improve everyday 
+                Software Engineer<br/> | Love to learn & improve everyday <span><AiFillSmile size="2.5rem"/></span>
             </SectionText>
-            <Button onClick={()=> window.location ="https://github.com/Lyndating"}>
+            <br/>
+            <br/>
+            <br/>
+            <Button onClick={()=> window.location ="/#projects"}>
                 Explore!
             </Button>
         </MainSection>
