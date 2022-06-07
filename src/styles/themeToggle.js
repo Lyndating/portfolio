2a8a947f0b29dd75cfbc9e5b5bdbb4d420792087
Ting Luo <shirleyayu@hotmail.com>
2022-06-07 10:20:44 +1000

    clean up code in theme togggle

--- src/styles/themeToggle.js
+++ src/styles/themeToggle.js
@@ -1,7 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import {MdBrightness5, MdBedtime} from 'react-icons/md';
 import styled from 'styled-components';
-import { ThemeProvider } from 'styled-components';
 
 const Div = styled.div `
     & svg: {
@@ -13,12 +12,9 @@ const Div = styled.div `
 
 
 const ThemeToggle = (props) => {
-    console.log(props);
     const [colorMode, setColorMode]= useState('dark');
     const changeModeHandler =()=>{
         setColorMode(colorMode==="dark"? "light" : "dark");
-        console.log(colorMode);
-        props.onClick(colorMode);
     }
     useEffect(()=>{
         document.body.dataset.theme = colorMode;
