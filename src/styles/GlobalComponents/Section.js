5fdff0e2831473a77727a372814abf3e1a1f6144
Ting Luo <shirleyayu@hotmail.com>
2022-06-17 22:55:01 +1000

    transfer color style from Section.js to global.js

--- src/styles/GlobalComponents/Section.js
+++ src/styles/GlobalComponents/Section.js
@@ -29,7 +29,6 @@ export const SectionTitle = styled.h2`
   line-height: ${(props) => props.main ? '72px' : '56px'};
   width: max-content;
   max-width: 100%;
-  background: linear-gradient(121.57deg, #FFFFFF 18.77%, rgba(255, 255, 255, 0.66) 60.15%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   margin-bottom: 16px;
@@ -57,7 +56,7 @@ export const SectionText = styled.p`
   line-height: 40px;
   font-weight: 300;
   padding-bottom: 3.6rem;
-  color: rgba(255, 255, 255, 0.5);
+
   @media ${(props) => props.theme.breakpoints.md} {
     max-width: 670px;
     font-size: 20px;
