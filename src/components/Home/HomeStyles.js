51698fb830ad099c8c393d183169f3f01c5db8bc
Ting Luo <shirleyayu@hotmail.com>
2022-06-22 21:32:41 +1000

    fix padding size for mobile screen

--- src/components/Home/HomeStyles.js
+++ src/components/Home/HomeStyles.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 
 export const MainSection = styled.div`
     width: 100%;
+    padding-left: 0;
+
+    @media ${(props) => props.theme.breakpoints.sm} {
+        padding-left: 20px;
+    }
 `
